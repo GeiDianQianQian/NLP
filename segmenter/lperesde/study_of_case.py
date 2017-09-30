ff7832479adb7967baa6673957ced929bfecffc8
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-09-30 01:43:08 -0700

    Printing file reversed (in order to apply our case)

--- segmenter/lperesde/study_of_case.py
+++ segmenter/lperesde/study_of_case.py
@@ -38,10 +38,10 @@ Pw  = Pdist(opts.counts1w)
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 # ignoring the dictionary provided in opts.counts
-with open(opts.input) as f:
-    for line in f:
-        utf8line = unicode(line.strip(), 'utf-8')
-        output = [i for i in utf8line]  # segmentation is one word per character in the input
-        #print " ".join(output)
+for line in reversed(open(opts.input).readlines()):
+    utf8line = unicode(line.strip(), 'utf-8')
+    for c in reversed(utf8line):
+        sys.stdout.write(c)
+    print ""
 sys.stdout = old
 
