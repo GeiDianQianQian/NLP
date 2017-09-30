1b2c587bab1a4f51d872ae5aed37f553c4d9d3ea
tmosharr <tmosharr@sfu.ca>
2017-09-30 10:31:32 -0400

    baseline implementation, accuracy=80%

--- segmenter/tmosharr/baseline.py
+++ segmenter/tmosharr/baseline.py
@@ -3,9 +3,9 @@ import sys, codecs, optparse, os, math
 from Queue import PriorityQueue
 
 optparser = optparse.OptionParser()
-optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('../data', 'count_1w.txt'), help="unigram counts")
-optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('../data', 'count_2w.txt'), help="bigram counts")
-optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('../data', 'input'), help="input file to segment")
+optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
+optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('data', 'count_2w.txt'), help="bigram counts")
+optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
 (opts, _) = optparser.parse_args()
 
 class Pdist(dict):
@@ -39,27 +39,13 @@ class Entry:
         self.log_prob=log_prob
         self.back_ptr=back_ptr
 
-class MyPriorityQueue(PriorityQueue):
-    def __init__(self):
-        PriorityQueue.__init__(self)
-        self.counter = 0
 
-    def __len__(self):
-        PriorityQueue.__len__(self)
-
-    def put(self, item, priority):
-        PriorityQueue.put(self, (priority, self.counter, item))
-        self.counter += 1
-
-    def get(self, *args, **kwargs):
-        _, _, item = PriorityQueue.get(self, *args, **kwargs)
-        return item
 
 
 # the default segmenter does not use any probabilities, but you could ...
 Pw  = Pdist(opts.counts1w)
-keys=Pw.keys()
-pq = MyPriorityQueue()
+keys= Pw.keys()
+pq  = PriorityQueue()
 
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
@@ -69,39 +55,59 @@ input=""
 count=0
 with open(opts.input) as f:
 
-    #getting the input ready
+    #getting the input ready, ommitting newline, joining together
     for line in f:
         utf8line = unicode(line.strip(), 'utf-8')
-        input+=utf8line
+        input=utf8line
+        #print(utf8line)
+        #initializing chart
+        finalindex=len(input)-1
+        chart = [None] * (finalindex+1)
 
-    finalindex=len(input)
-    chart = [None] * finalindex
+        #initializing priorityqueue, gather candidates for first word
+        inserted=0
+        for first_word in keys:
+            if(first_word==input[0:len(first_word)]):
+                entry=Entry(first_word,0,math.log(Pw(first_word),2),None)
+                pq.put((0,entry))
+                inserted+=1
+        if(inserted==0):
+            first_word=input[0]
+            entry = Entry(first_word,0,math.log(Pw(first_word),2), None)
+            pq.put((0,entry))
 
-    #initializing priorityqueue
-    for key in keys:
-        if(key.startswith(input[0])):
-            entry=Entry(key,0,math.log(Pw(key),2),None)
-            pq.put(entry,0)
+        #recursive
+        while pq.empty()==False:
 
-    #recursive
-    while pq.empty()==False:
-        entry=pq.get()
-        endindex=entry.start_pos+len(entry.word)-1
-        if(chart[endindex]!=None):
-            if(chart[endindex].log_prob<entry.log_prob):
-                chart[endindex]=entry
+            #try to insert a word
+            _,entry=pq.get()
+            endindex=entry.start_pos+len(entry.word)-1
+            if(chart[endindex]!=None):
+                if(chart[endindex].log_prob<entry.log_prob):
+                    chart[endindex]=entry
+                else:
+                    continue
             else:
-                continue
-        else:
-            chart[endindex] = entry
-        new_start=endindex+1
-        for new_word in keys:
-            if (new_word.startswith(input[new_start])):
-                new_entry = Entry(new_word, new_start, entry.log_prob+math.log(Pw(new_word), 2), entry)
-                pq.put(new_entry, new_start)
+                chart[endindex] = entry
 
-    for el in chart:
-        if el != None and el.word != None:
-            print(el.word+" ")
+            #gather candidates for next word
+            next_start=endindex+1
+            inserted = 0
+            for next_word in keys:
+                if (next_word==(input[next_start:next_start+len(next_word)])):
+                    next_entry = Entry(next_word, next_start, entry.log_prob+math.log(Pw(next_word), 2), entry)
+                    pq.put((next_start,next_entry))
+                    inserted+=1
+            if((inserted==0) and (next_start<=finalindex)):
+                next_word=input[next_start]
+                next_entry = Entry(next_word, next_start, entry.log_prob + math.log(Pw(next_word), 2), entry)
+                pq.put((next_start, next_entry))
 
+        wordlist=[]
+        current_entry=chart[finalindex]
+        while(current_entry!=None):
+            wordlist.append(current_entry.word)
+            current_entry=current_entry.back_ptr
+        wordlist.reverse()
+        print " ".join(wordlist)
 sys.stdout = old
