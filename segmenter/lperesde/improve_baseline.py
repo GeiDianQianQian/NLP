ec7e5b9c5978dceca73a843584592ed089ba99ba
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-09-30 23:30:23 -0700

    Improving slightly the code

--- segmenter/lperesde/improve_baseline.py
+++ segmenter/lperesde/improve_baseline.py
@@ -1,4 +1,3 @@
-
 import sys, codecs, optparse, os, math
 from Queue import PriorityQueue
 
@@ -31,85 +30,87 @@ class Pdist(dict):
         else: return None
 
 class Entry:
-    #Entry to be used
-
+    # Entry to be used
     def __init__(self,word,start_pos,log_prob,back_ptr):
         self.word=word
         self.start_pos=start_pos
         self.log_prob=log_prob
         self.back_ptr=back_ptr
 
-
-
-
 # the default segmenter does not use any probabilities, but you could ...
 Pw  = Pdist(opts.counts1w)
 keys= Pw.keys()
 pq  = PriorityQueue()
 
+def buildPossibilities(chart):
+    #recursive
+    while pq.empty() == False:
+        # try to insert a word
+        _,entry = pq.get()
+        endindex = entry.start_pos + len(entry.word) - 1
+        if(chart[endindex] != None):
+            if(chart[endindex].log_prob < entry.log_prob):
+                chart[endindex] = entry
+            else:
+                continue
+        else:
+            chart[endindex] = entry
+
+        # gather candidates for next word
+        next_start=endindex+1
+        inserted = 0
+        for allowed_length in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+            next_word = input[next_start:next_start+allowed_length]
+            if(next_word in Pw):
+                next_entry = Entry(next_word,
+                                   next_start,
+                                   entry.log_prob + math.log(Pw(next_word), 2),
+                                   entry)
+                pq.put((next_start,next_entry))
+                inserted += 1
+
+        if((inserted == 0) and (next_start <= finalindex)):
+            next_word = input[next_start]
+            next_entry = Entry(next_word, next_start, entry.log_prob + math.log(Pw(next_word), 2), entry)
+            pq.put((next_start, next_entry))
+
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
-
-# ignoring the dictionary provided in opts.counts
 input=""
 count=0
 with open(opts.input) as f:
 
-    #getting the input ready, ommitting newline, joining together
+    # getting the input ready, ommitting newline, joining together
     for line in f:
-        utf8line = unicode(line.strip(), 'utf-8')
-        input=utf8line
-        #print(utf8line)
+        input = unicode(line.strip(), 'utf-8')
+
         #initializing chart
-        finalindex=len(input)-1
-        chart = [None] * (finalindex+1)
+        finalindex = len(input) - 1
+        chart = [None] * (finalindex + 1)
 
         #initializing priorityqueue, gather candidates for first word
-        inserted=0
+        inserted = 0
         for allowed_length in [1,2,3,4,5,6,7,8,9,10]:
-            first_word=input[0:allowed_length]
+            first_word = input[0:allowed_length]
             if(first_word in Pw):
-                entry=Entry(first_word,0,math.log(Pw(first_word),2),None)
+                entry = Entry(first_word,0,math.log(Pw(first_word),2),None)
                 pq.put((0,entry))
-                inserted+=1
-        if(inserted==0):
-            first_word=input[0]
+                inserted += 1
+
+        if(inserted == 0):
+            first_word = input[0]
             entry = Entry(first_word,0,math.log(Pw(first_word),2), None)
             pq.put((0,entry))
 
-        #recursive
-        while pq.empty()==False:
+        while pq.empty() == False:
+            buildPossibilities(chart)
 
-            #try to insert a word
-            _,entry=pq.get()
-            endindex=entry.start_pos+len(entry.word)-1
-            if(chart[endindex]!=None):
-                if(chart[endindex].log_prob<entry.log_prob):
-                    chart[endindex]=entry
-                else:
-                    continue
-            else:
-                chart[endindex] = entry
-
-            #gather candidates for next word
-            next_start=endindex+1
-            inserted = 0
-            for allowed_length in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
-                next_word = input[next_start:next_start+allowed_length]
-                if(next_word in Pw):
-                    next_entry = Entry(next_word, next_start, entry.log_prob+math.log(Pw(next_word), 2), entry)
-                    pq.put((next_start,next_entry))
-                    inserted+=1
-            if((inserted==0) and (next_start<=finalindex)):
-                next_word=input[next_start]
-                next_entry = Entry(next_word, next_start, entry.log_prob + math.log(Pw(next_word), 2), entry)
-                pq.put((next_start, next_entry))
-
-        wordlist=[]
-        current_entry=chart[finalindex]
-        while(current_entry!=None):
+        wordlist = []
+        current_entry = chart[finalindex]
+        while(current_entry != None):
             wordlist.append(current_entry.word)
-            current_entry=current_entry.back_ptr
+            current_entry = current_entry.back_ptr
         wordlist.reverse()
         print " ".join(wordlist)
+
 sys.stdout = old
