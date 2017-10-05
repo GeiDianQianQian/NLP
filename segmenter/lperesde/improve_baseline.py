40c90c8c4e7ea0409b80bd10d6631aad5fbbe9ea
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-10-05 00:27:15 -0700

    Changing basefile for 1 word counting

--- segmenter/lperesde/improve_baseline.py
+++ segmenter/lperesde/improve_baseline.py
@@ -3,7 +3,7 @@ import sys, codecs, optparse, os, math
 from Queue import Queue
 
 optparser = optparse.OptionParser()
-optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('wseg_data', 'count_wseg.txt'), help="unigram counts")
+optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('wseg_data', 'count_wseg_1w.txt'), help="unigram counts")
 optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('data', 'count_2w.txt'), help="bigram counts")
 optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
 (opts, _) = optparser.parse_args()
