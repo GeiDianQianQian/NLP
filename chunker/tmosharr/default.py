c0b4539ea6bc82b6e56f2eec7be89209996e483e
tmosharr <tmosharr@sfu.ca>
2017-10-14 11:03:39 -0800

    implemented baseline, accuracy 91%

--- chunker/tmosharr/default.py
+++ chunker/tmosharr/default.py
@@ -35,14 +35,77 @@ from collections import defaultdict
 def perc_train(train_data, tagset, numepochs):
     feat_vec = defaultdict(int)
     # insert your code here
+    #print(train_data)
+    epoch=0
+
+    while(epoch<numepochs):
+        mistakes = 0
+        correct = 0
+        for sentence_data in train_data:
+            words=[]
+            postags=[]
+            truetags=[]
+            label_list=sentence_data[0]
+            feat_list=sentence_data[1]
+            for label in label_list:
+                (word,postag,chunktag)=label.split(" ")
+                words.append(word)
+                postags.append(postag)
+                truetags.append(chunktag)
+            tagset = perc.read_tagset(opts.tagsetfile)
+            default_tag=tagset[0]
+            argmaxtags=perc.perc_test(feat_vec, label_list, feat_list, tagset, default_tag)
+            feat_index = 0
+            i=0
+            for word in words:
+                (feat_index, feats_for_this_word) = perc.feats_for_word(feat_index, feat_list)
+                #print(len(feats_for_this_word))
+                argmax=argmaxtags[i]
+                tru=truetags[i]
+                if(argmax==tru):
+                    i+=1
+                    continue
+                for f in feats_for_this_word:
+                    wrongkey=f,argmax
+                    rightkey=f,tru
+                    feat_vec[wrongkey]=feat_vec.get(wrongkey,0)-1
+                    feat_vec[rightkey]=feat_vec.get(rightkey,0)+1
+                i+=1
+            i = 0
+            for word in words:
+                argmax = argmaxtags[i]
+                tru = truetags[i]
+                if(argmax==tru):
+                    i+=1
+                    correct+=1
+                    continue
+                else:
+                    mistakes+=1
+                argmaxprev="B:"
+                truprev="B:"
+                if(i==0):
+                    argmaxprev+="B_-1"
+                    truprev+="B_-1"
+                else:
+                    argmaxprev+= argmaxtags[i-1]
+                    truprev+=truetags[i-1]
+                wrongkey=argmaxprev,argmax
+                rightkey=truprev,tru
+                feat_vec[wrongkey] = feat_vec.get(wrongkey, 0) - 1
+                feat_vec[rightkey] = feat_vec.get(rightkey, 0) + 1
+                i += 1
+
+        print(mistakes)
+        print(correct)
+        epoch+=1
     # please limit the number of iterations of training to n iterations
     return feat_vec
 
 if __name__ == '__main__':
     optparser = optparse.OptionParser()
     optparser.add_option("-t", "--tagsetfile", dest="tagsetfile", default=os.path.join("data", "tagset.txt"), help="tagset that contains all the labels produced in the output, i.e. the y in \phi(x,y)")
-    optparser.add_option("-i", "--trainfile", dest="trainfile", default=os.path.join("data", "train.txt.gz"), help="input data, i.e. the x in \phi(x,y)")
-    optparser.add_option("-f", "--featfile", dest="featfile", default=os.path.join("data", "train.feats.gz"), help="precomputed features for the input data, i.e. the values of \phi(x,_) without y")
+    optparser.add_option("-i", "--trainfile", dest="trainfile", default=os.path.join("data", "train.dev"), help="input data, i.e. the x in \phi(x,y)")
+    optparser.add_option("-f", "--featfile", dest="featfile", default=os.path.join("data", "train.feats.dev"), help="precomputed features for the input data, i.e. the values of \phi(x,_) without y")
     optparser.add_option("-e", "--numepochs", dest="numepochs", default=int(10), help="number of epochs of training; in each epoch we iterate over over all the training examples")
     optparser.add_option("-m", "--modelfile", dest="modelfile", default=os.path.join("data", "default.model"), help="weights for all features stored on disk")
     (opts, _) = optparser.parse_args()
