316662c345a56372199d09e5b91f6b3724780db6
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-11-12 19:33:31 -0800

    All files updated

--- aligner/answer/align.py
+++ aligner/answer/align.py
@@ -19,35 +19,30 @@ e_data = "%s.%s" % (os.path.join(opts.datadir, opts.fileprefix), opts.english)
 if opts.logfile:
     logging.basicConfig(filename=opts.logfile, filemode='w', level=logging.INFO)
 
-bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:opts.num_sents]]
-f_count = defaultdict(int)
-e_count = defaultdict(int)
+bitext = [[sentence.lower().strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:opts.num_sents]]
 
-#sys.stderr.write("Creating French and English Dictionary, storing counts for each words")
+sys.stderr.write("\nGetting size vocabulary f")
+count_f = defaultdict(float)
+count_e = defaultdict(float)
 for (n, (f, e)) in enumerate(bitext):
-  e.append("n_wd")
-  for f_i in set(f):
-    f_count[f_i] += 1
-  for e_i in set(e):
-    e_count[e_i] += 1
+    for f_i in set(f):
+        count_f[f_i] += 1
+    for e_j in set(e):
+        count_e[e_j] += 1
+    if n % 500 == 0:
+        sys.stderr.write(".")
 
-#sys.stderr.write("Determining vocabulary size of French and English Dictionary")
-v_f = float(len(f_count.keys()))
-v_e = float(len(e_count.keys()))
+sys.stderr.write("\nApplying IBM model 1")
+v_f = float(len(count_f.keys()))
+v_e = float(len(count_e.keys()))
 small = 0.01
-
 t1 = defaultdict(float)
 t2 = defaultdict(float)
-
 k = 0
-sys.stderr.write("\nTraining IBM Model 1 (no nulls) with Expectation Maximization...")
 
 while (k < opts.num_eps):
-
-    sys.stderr.write("\nIteration " + str(k))
     k += 1
-
-    sys.stderr.write("\nM step ")
+    sys.stderr.write("\nIteration " + str(k))
     count_e  = defaultdict(float)
     count_fe = defaultdict(float)
     count_f  = defaultdict(float)
@@ -55,87 +50,87 @@ while (k < opts.num_eps):
 
     for(n, (f, e)) in enumerate(bitext):
         e.append("n_wd")
+        f.append("n_wd")
         for f_i in set(f):
-
             z = 0.0
             for e_j in set(e):
-                if(k == 1):
-                    z += 1.0 / v_f
-                else:
-                    z += t1[(f_i,e_j)]
+                z += t1[(f_i, e_j)] if k > 1 else 1.0 / v_f
 
             c = 0.0
             for e_j in set(e):
-                if (k == 1):
-                    c = (1.0/v_f)/z
-                else:
-                    c = t1[(f_i, e_j)]/z
-                count_fe[(f_i,e_j)] = count_fe[(f_i,e_j)] + c
+                c = t1[(f_i, e_j)] / z if k > 1 else 1.0
+                count_fe[(f_i, e_j)] = count_fe[(f_i, e_j)] + c
                 count_e[(e_j)] = count_e[e_j] + c
-
+        
         for e_i in set(e):
-
             z = 0.0
             for f_j in set(f):
-                if(k == 1):
-                    z += 1.0 / v_e
-                else:
-                    z += t2[(e_i,f_j)]
+                z += t2[(e_i, f_j)] if k > 1 else 1.0 / v_e
 
             c = 0.0
             for f_j in set(f):
-                if (k == 1):
-                    c = (1.0/v_e)/z
-                else:
-                    c = t2[(e_i, f_j)]/z
-                count_ef[(e_i,f_j)] = count_ef[(e_i,f_j)] + c
+                c = t2[(e_i, f_j)] / z if k > 1 else 1.0
+                count_ef[(e_i, f_j)] = count_ef[(e_i, f_j)] + c
                 count_f[(f_j)] = count_f[f_j] + c
 
-    sys.stderr.write("\n"+str(len(count_fe.keys()))+" "+str(len(count_ef.keys())))
-    sys.stderr.write("\nE step ")
+        if n % 500 == 0:
+            sys.stderr.write(".")
 
-    for (n, (f,e)) in enumerate(count_fe.keys()):
-        t1[(f,e)]=(count_fe[(f,e)]+small)/(count_e[e]+small*v_f)
+    sys.stderr.write("\nGetting translation rate")
 
-
-    for (n, (e,f)) in enumerate(count_ef.keys()):
-        t2[(e,f)]=(count_ef[(e,f)]+small)/(count_f[f]+small*v_e)
-
+    for (n, (f, e)) in enumerate(count_fe.keys()):
+        t1[(f, e)] = (count_fe[(f, e)] + small) / (count_e[e] + small * v_f)
+    for (n, (e, f)) in enumerate(count_ef.keys()):
+        t2[(e, f)] = (count_ef[(e, f)] + small) / (count_f[f] + small * v_e)
 
 sys.stderr.write("\nAligning ")
 
 for(n, (f, e)) in enumerate(bitext):
     e.append("n_wd")
+    f.append("n_wd")
     result = defaultdict(list)
 
     for (i, e_i) in enumerate(e):
-
-        bestp = 0
-        bestj = None
-
-        for (j, f_j) in enumerate(f):
-            if (t2[(e_i, f_j)] > bestp):
-                bestp = t2[(e_i, f_j)]
-                bestj = f_j
-
-        result[bestj].append(e_i)
-
-    for (i,f_i) in enumerate(f):
-
         bestp = 0
         bestj = 0
-        best  = None
-
-        for (j,e_j) in enumerate(e):
-            if(t1[(f_i,e_j)] > bestp):
-                bestp = t1[(f_i,e_j)]
+        for (j, f_j) in enumerate(f):
+            curp = t2[(e_i, f_j)]
+            if(curp > bestp):
+                bestp = curp
                 bestj = j
-                best  = e_j
+                word = f_j
+            elif(curp == bestp):
+                if (abs(i - j) < abs(i - bestj)):
+                    bestp = curp
+                    bestj = j
+                    word = f_j
 
-        if(f_i in result.keys()):
-            if(best in result[f_i]) and best != "n_wd":
-                sys.stdout.write("%i-%i " % (i, bestj))
+        # align e_aj to f_j
+        result[i] = bestj
+
+    for (i, f_i) in enumerate(f):
+        bestp = 0
+        bestj = 0
+        word  = None
+        for (j, e_j) in enumerate(e):
+            curp = t1[(f_i, e_j)]
+            if(curp > bestp):
+                bestp = curp
+                bestj = j
+                word = e_j
+            elif(curp == bestp):
+                if (abs(i - j) < abs(i - bestj)):
+                    bestp = curp
+                    bestj = j
+                    word = e_j
+
+        # if f_j matches to e_aj, then we are sure about it
+        if result[bestj] == i and word != "n_wd":
+            sys.stdout.write("%i-%i " % (i, bestj))
+        # otherwise we are not so sure about it
+        # elif word != "n_wd":
+        #     sys.stdout.write("%i-%i " % (i, bestj))
 
     sys.stdout.write("\n")
-
-    #sys.stdout.write("\n")
+    if n % 500 == 0:
+        sys.stderr.write(".")
