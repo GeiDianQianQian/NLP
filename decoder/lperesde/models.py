19c76332e5bd973f2d06f9d221335fc7c9c452e1
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-11-19 21:09:03 -0800

    Implemented baseline

--- decoder/lperesde/models.py
+++ decoder/lperesde/models.py
@@ -17,9 +17,9 @@ def TM(filename, k):
   for line in open(filename).readlines():
     (f, e, logprob) = line.strip().split(" ||| ")
     tm.setdefault(tuple(f.split()), []).append(phrase(e, float(logprob)))
-  for f in tm: # prune all but top k translations
-    tm[f].sort(key=lambda x: -x.logprob)
-    del tm[f][k:] 
+  #for f in tm: # prune all but top k translations
+    #tm[f].sort(key=lambda x: -x.logprob)
+    #del tm[f][k:] 
   return tm
 
 # # A language model scores sequences of English words, and must account
