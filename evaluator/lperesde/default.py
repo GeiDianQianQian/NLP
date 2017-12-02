6d584629521abb0e185ab4006951d507f0cff73b
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-12-02 04:23:59 -0800

    Improved a little bit

--- evaluator/lperesde/default.py
+++ evaluator/lperesde/default.py
@@ -3,6 +3,7 @@ import argparse # optparse is deprecated
 from itertools import islice # slicing for iterators
 import numpy as np
 from collections import Counter
+import nltk
 
 parser = argparse.ArgumentParser(description='Evaluate translation hypotheses.')
 parser.add_argument('-i', '--input', default='../data/hyp1-hyp2-ref', help='input file (default data/hyp1-hyp2-ref)')
@@ -32,27 +33,27 @@ def get_ngrams(sentence, ref1, ref2, vc1, vc2):
         h1_ngrams = [tuple(ref1[i:i+n]) for i in xrange(len(ref1)+1-n)]
         h2_ngrams = [tuple(ref2[i:i+n]) for i in xrange(len(ref2)+1-n)]
 
+        # save precison, score and f1 for ngrams
         (vc1[n-1], vc1[n+3], vc1[n+7]) = matches(h1_ngrams, e_ngrams)
         (vc2[n-1], vc2[n+3], vc2[n+7]) = matches(h2_ngrams, e_ngrams)
 
+    # average of ngrams
     vc1[12] = (vc1[0]+vc1[1]+vc1[2]+vc1[3])/4
     vc2[12] = (vc2[0]+vc2[1]+vc2[2]+vc2[3])/4
 
     return (vc1, vc2)
 
 def get_words(sentence, ref1, ref2, vc1, vc2):
-    for i,v in enumerate(vc1):
-        vc1[i] = 0 if not v else v
-    for i,v in enumerate(vc2):
-        vc2[i] = 0 if not v else v
+    # no-op
     return (vc1, vc2)
 
 def get_pos(sentence, ref1, ref2, vc1, vc2):
+    # no-op
     return (vc1, vc2)
 
 for h1, h2, e in islice(sentences(), opts.num_sentences):
-    vc1 = [None] * 32 # feature vector h1
-    vc2 = [None] * 32 # feature vector h2
+    vc1 = [0] * 32 # feature vector h1
+    vc2 = [0] * 32 # feature vector h2
     (vc1, vc2) = get_ngrams(e, h1, h2, vc1, vc2)
     (vc1, vc2) = get_words(e, h1, h2, vc1, vc2)
     (vc1, vc2) = get_pos(e, h1, h2, vc1, vc2)
